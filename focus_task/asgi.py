d7e3ab7216a393f3b97740b4c162f867de850198
BJMinhNhut <bjminhnhut@gmail.com>
2024-11-01 23:35:40 +0700

    rename focustask to focus_task

--- focus_task/asgi.py
+++ focus_task/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for focustask project.
+ASGI config for focus_task project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "focustask.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "focus_task.settings")
 
 application = get_asgi_application()
--- focus_task/wsgi.py
+++ focus_task/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for focustask project.
+WSGI config for focus_task project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "focustask.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "focus_task.settings")
 
 application = get_wsgi_application()
