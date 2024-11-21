0d26e423bc768004f03553e492d486ab0d38d66d
ThisIsNotDat <datqt2407@gmail.com>
2024-11-21 12:17:38 +0700

    Reapply "Modifying many-to-many relationship"
    This reverts commit 5bdf0927d0e766229e6e1e6a8e9ebea62a1b22e3.

--- todolist/models.py
+++ todolist/models.py
@@ -1,25 +1,31 @@
+from pyexpat import model
 from re import M
 import re
 from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Label(models.Model):
+    name = models.CharField(max_length=100)
+class RepeatedMode(models.Model):
+    name = models.CharField(max_length = 10)
+    dayType = models.TextChoices("Day of Week", "Sunday Monday Tuesday Wednesday Thursday Friday Saturday")
+    startDuration = models.DateField("Start of Repeat")
+    endDuration = models.DateField("End of Repeat")
+
+
 class Task(models.Model):
     taskID = models.CharField(max_length=100, primary_key=True)
     taskName = models.CharField(max_length=200)
     dueDate = models.DateTimeField("Due date")
     reminder = models.DateTimeField("Reminding date", null = True)
-    repeatOne = models.BooleanField(default = False)
-    repeatEveryDay = models.BooleanField(default = False)
-    repeatEveryWeek = models.BooleanField(default = False)
     description = models.CharField(max_length = 500, null = True)
     isDoneState = models.BooleanField(default = False)
+    priority = models.TextChoices("Priority", "Red Yellow Green")
     createdDate = models.DateTimeField("Created date", default = timezone.now)
-    #label = models.Many
+    labels = models.ManyToManyField(Label)
+    repeatModeList = models.ManyToManyField(RepeatedMode)
 
-class Label(models.Model):
-    taskID = models.ForeignKey(Task, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
 
 
 class FileAttachment(models.Model):
@@ -33,12 +39,13 @@ class URLAttachment(models.Model):
 class TaskContainer(models.Model):
     createdDate = models.DateTimeField("Created date", default = timezone.now)
     containerName = models.CharField(max_length=100)
+    tasksList = models.ManyToManyField(Task)
 
-class TaskRelation(models.Model):
-    taskContainer = models.ForeignKey(TaskContainer, on_delete=models.CASCADE)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+class Archive(models.Model):
+    containerList = models.ManyToManyField(TaskContainer)
 
 class TaskSubrelation(models.Model):
     mainTask = models.ForeignKey(Task , related_name="Main_task", on_delete=models.CASCADE)
     subTask = models.ForeignKey(Task, related_name="Sub_task",  on_delete=models.CASCADE)
+
     
