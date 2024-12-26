f1712766f3713c27ed96e0ae6dda1989b982657e
Monarchuwu <75823146+Monarchuwu@users.noreply.github.com>
2024-12-26 22:26:08 +0700

    AddTask textbox for TodayPage

--- frontend/src/pages/TodayPage.js
+++ frontend/src/pages/TodayPage.js
@@ -5,6 +5,11 @@ import { callAPITemplate } from '../utils';
 import React from 'react';
 
 function TodayPage({ setViewTaskDetailID, updateTaskAttrs, setUpdateTaskAttrs, setSuggestTaskList }) {
+    // State variables for adding new task
+    const [newTaskName, setNewTaskName] = React.useState("");
+    const [newTaskDescription, setNewTaskDescription] = React.useState("");
+    const [projectDefaultID, setProjectDefaultID] = React.useState(null); // projectID of projectName is '' (default project)
+    const [sectionDefaultID, setSectionDefaultID] = React.useState(null); // sectionID of sectionName is '' (default section)
     // State variables for rendering the tree
     const [taskList, setTaskList] = React.useState([]); // a list of tasks that will be rendered
     const [taskStatusMap, setTaskStatusMap] = React.useState({}); // Checkbox status of tasks
@@ -13,6 +18,45 @@ function TodayPage({ setViewTaskDetailID, updateTaskAttrs, setUpdateTaskAttrs, s
 
 
     // API call functions
+    const callGetProjectByNameAPI = async (name) => {
+        const authToken = localStorage.getItem('authToken');
+        return await callAPITemplate(
+            `${process.env.REACT_APP_API_URL}/project/get_by_name`,
+            JSON.stringify({ "authenticationToken": authToken, "projectName": name }),
+        )
+    };
+    const callGetSectionByNameAPI = async (projectID, name) => {
+        const authToken = localStorage.getItem('authToken');
+        return await callAPITemplate(
+            `${process.env.REACT_APP_API_URL}/section/get_by_name`,
+            JSON.stringify({ "authenticationToken": authToken, "projectID": projectID, "sectionName": name }),
+        )
+    };
+    const callAddTaskAPI = async (name, parentID, dueDate = null, priority = null, status = null, description = null, inTodayDate = new Date().toISOString()) => {
+        const authToken = localStorage.getItem('authToken');
+        var payload = {
+            "authenticationToken": authToken,
+            "name": name,
+            "parentID": parentID,
+            "dueDate": dueDate,
+            "priority": priority,
+            "status": status,
+            "description": description,
+            "inTodayDate": inTodayDate,
+        }
+        payload = Object.fromEntries(
+            Object.entries(payload).filter(([_, value]) => value !== null)
+        );
+        callAPITemplate(
+            `${process.env.REACT_APP_API_URL}/task/add`,
+            JSON.stringify(payload),
+            (data) => {
+                setNewTaskName("");
+                setNewTaskDescription("");
+                fetchTodoList();
+            }
+        )
+    }
     const callDeleteTodoItemAPI = async (itemID) => {
         const authToken = localStorage.getItem('authToken');
         callAPITemplate(
@@ -73,6 +117,7 @@ function TodayPage({ setViewTaskDetailID, updateTaskAttrs, setUpdateTaskAttrs, s
     React.useEffect(() => {
         fetchTodoList();
     }, [updateTaskAttrs]);
+
     // Update checkbox status smoothly with debouncing (50ms)
     React.useEffect(() => {
         const timeout = setTimeout(() => {
@@ -84,6 +129,7 @@ function TodayPage({ setViewTaskDetailID, updateTaskAttrs, setUpdateTaskAttrs, s
         return () => clearTimeout(timeout);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [debounceStatus]);
+
     // Cleanup function
     React.useEffect(() => {
         return () => {
@@ -93,10 +139,42 @@ function TodayPage({ setViewTaskDetailID, updateTaskAttrs, setUpdateTaskAttrs, s
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    // Get the default project and section IDs
+    React.useEffect(() => {
+        const loadDefaultProjectSection = async () => {
+            const projectItem = JSON.parse(await callGetProjectByNameAPI(''));
+            setProjectDefaultID(projectItem.itemID);
+            const sectionItem = JSON.parse(await callGetSectionByNameAPI(projectItem.itemID, ''));
+            setSectionDefaultID(sectionItem.itemID);
+        }
+        loadDefaultProjectSection();
+    }, [taskList]);
+
 
     return (
         <div>
             <h1>Today</h1>
+            {/* Add Task */}
+            {projectDefaultID && sectionDefaultID && (
+                <div>
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        value={newTaskName}
+                        onChange={(e) => setNewTaskName(e.target.value)}
+                    />
+                    <br />
+                    <label>Description</label>
+                    <input
+                        type="text"
+                        value={newTaskDescription}
+                        onChange={(e) => setNewTaskDescription(e.target.value)}
+                    />
+                    <button onClick={() => { setNewTaskName(""); setNewTaskDescription(""); }}>Cancel</button>
+                    <button onClick={() => callAddTaskAPI(newTaskName, sectionDefaultID, undefined, undefined, undefined, newTaskDescription)}>Add</button>
+                </div>
+            )}
+            {/* Suggest Task List button */}
             <button onClick={() => setSuggestTaskList(true)}>Suggestions</button>
             {/* Render Task List */}
             <ul className={styles.taskList}>
